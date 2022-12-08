f02dc0e99f9711158ae409e95e406812cc305d83
Fidelis-7 <abanumfidelis90@gmail.com>
2022-12-08 22:55:52 +0100

    pycharm.py

--- pycharm.py
+++ pycharm.py
@@ -1,10 +1,26 @@
 class Flight():
-    def __init__(self, capacity, name):
+    def __init__(self, capacity):
         self.capacity = capacity
-        self.name = name
+        self.passenger = []
+
+    
+    def add_passenger(self, name):
+        if self.open_seats():
+            self.passenger.append(name)
+            return True
+        else:
+            print("pls all seats are taken")
+            return False
         
-    def Pname(self):
-        print(f"the name of the first passenger is {self.name}")
         
-flight = Flight(2, "fidelis")
-flight.Pname()
+    def open_seats(self):
+        return self.capacity - len(self.passenger)
+        
+flight = Flight(3)
+people = ["fidelis", "obed", "nams", "Debbie"]
+for person in people:
+    success = flight.add_passenger(person)
+    if success:
+        print(f"congrats {person} added")
+    else:
+        print(f"space is full {person}")
